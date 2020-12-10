e8e80f634b70aae2d938367057a26ca9ece132ba
nathan.bonnet@epitech.eu <nathan.bonnet@epitech.eu>
2020-12-10 10:30:28 +0100

    (feat): init + server communication

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <Router>
+      
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/linkApi.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+const servAddr = 'http://localhost:3080/';
+
+export async function getBookmarks () {
+  let res;
+
+  await axios.get(`${servAddr}bookmark/`)
+    .then((response) => { res = response.data; return response.status })
+    .catch((err) => err);
+  return res;
+}
+
+export async function getBookmarkInfo (id) {
+  let res
+
+  await axios.get(`${servAddr}bookmark/${id}`)
+    .then((response) => {res = response.data; return response.status})
+    .catch((err) => err)
+  return res;
+}
+
+export async function putNewTags (id, tags) {
+  let res
+
+  await axios.put(`${servAddr}bookmark/${id}`, {tags})
+    .then((response) => {res = response.data; return response.status})
+    .catch((err) => err)
+  return res;
+}
+
+export async function deleteBookmark (id) {
+  let res
+
+  await axios.delete(`${servAddr}bookmark/${id}`)
+    .then((response) => {res = response.data; return response.status})
+    .catch((err) => err)
+  return res;
+}
+
+export async function postNewImgBookmark (url) {
+  let res;
+
+  await axios.post(`${servAddr}bookmark/img/`, {url})
+    .then((response) => {res = response.data; return response.status})
+    .catch((err) => err)
+  return res;
+}
+
+export async function postNewVideoBookmark(url) {
+  let res;
+
+  await axios.post(`${servAddr}bookmark/video/`, {url})
+    .then((response) => {res = response.data; return response.status})
+    .catch((err) => err)
+  return res;
+}
+
